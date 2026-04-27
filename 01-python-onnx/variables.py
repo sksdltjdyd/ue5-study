c8c8e0b49f8e72826181d529b4edde8483e61f20
SUROM <62508307+sksdltjdyd@users.noreply.github.com>
2026-04-28 07:58:33 +0900

    Create variables.py

---
+++ 01-python-onnx/variables.py
@@ -0,0 +1,19 @@
+# number
+a = 1
+b = 2.5
+c = a + b
+print(c)
+
+# string
+d = "hello"
+e = "12.4"
+f = d + " number is " + e
+g = 'hello"dsde'
+print(f)
+print(g)
+
+# list
+h = [1, 2, 3, 4, 5]
+i = ["a", "b", "c", "d", "e"]
+print(h)
+print(i)
