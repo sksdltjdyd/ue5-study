0c8e8739a280ddcea29a7f20de33ee5cfb60a8ef
SUROM <62508307+sksdltjdyd@users.noreply.github.com>
2026-04-28 08:34:24 +0900

    Update variables.py

--- 01-python-onnx/variables.py
+++ 01-python-onnx/variables.py
@@ -10,13 +10,31 @@ e = "12.4"
 f = d + " number is " + e
 g = 'hello"dsde'
 tail = "tail"
+word = "i have %d %s" %(3, "dogs") # string formatting
+name = "Lee"
+age = 30
+one = f"My name is {name} and I am {age} years old." # f-string
 print(f)
 print(g)
 print(tail[0:2]) # indexing
-
+print(word)
+print(one)
+print(tail.upper()) # string metho
+print(tail.count("a")) # count occurrences of "a"
+print(tail.replace("a", "o")) # replace "a" with "o"
 
 # list
 h = [1, 2, 3, 4, 5]
 i = ["a", "b", "c", "d", "e"]
+del h[0] # delete first element
+j = [1, "a", 2.5, [1, 2], {"key": "value"}] # list can contain different types
 print(h)
 print(i)
+print(j)
+print(len(h))
+del j[2:]
+print(j)
+h.append(6) # add element to end of list
+print(h)
+h.sort(reverse=True) # sort list in reverse order
+print(h)
