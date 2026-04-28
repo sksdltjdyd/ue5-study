ba60d6f015910ecde943ca5c375bb23532ff438b
SUROM <62508307+sksdltjdyd@users.noreply.github.com>
2026-04-29 08:18:11 +0900

    Update loop.py

--- 01-python-onnx/loop.py
+++ 01-python-onnx/loop.py
@@ -4,3 +4,41 @@ while tree < 10:
     tree += 1
     if tree == 10:
         print("tree is 10, stop counting")
+
+prompt = """
+1. Add
+2. del
+3. list
+4. quit
+Enter number: """
+
+number = 0
+while number != 4:
+    number = int(input(prompt))
+    if number == 1:
+        print("Add")
+    elif number == 2:
+        print("del")
+    elif number == 3:
+        print("list")
+    elif number == 4:
+        print("quit")
+    else:
+        print("wrong number")
+
+print ("program end")
+
+while True:
+    number = int(input(prompt))
+    if number == 1:
+        print("Add")
+    elif number == 2:
+        print("del")
+    elif number == 3:
+        print("list")
+    elif number == 4:
+        break
+    else:
+        print("wrong number")
+
+print ("program end")
