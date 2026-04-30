5f20d087c19053c290a40472017064539b9cf858
SUROM <62508307+sksdltjdyd@users.noreply.github.com>
2026-05-01 08:40:55 +0900

    Add files via upload

---
+++ 01-python-onnx/basic_operation_expression_variables.py
@@ -0,0 +1,13 @@
+# operation
+# a와 b라는 변수에 값을 할당하고 사칙연산을 통해 또 다른 변수에 결과를 넣어준다
+# =라는 expression은 오른쪽의 값을 왼쪽 변수에 할당한다는 의미
+a = 10
+b = 20
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+g = a % b
+h = a // b # 몫만 구함
+i = a ** 3 # 제곱
+print(a, b, c, d, e, f, g, h, i)
