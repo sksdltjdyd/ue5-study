53beaeff176f1d3ab893d32e8748ec7c1c0c0a8f
SUROM <62508307+sksdltjdyd@users.noreply.github.com>
2026-06-08 09:04:29 +0900

    v3

--- ch6 - unreal-advanced1/UnrealProject/UnrealStudy/Source/UnrealStudy/Private/Bird.cpp
+++ ch6 - unreal-advanced1/UnrealProject/UnrealStudy/Source/UnrealStudy/Private/Bird.cpp
@@ -4,6 +4,9 @@
 #include "Bird.h"
 #include "Components/CapsuleComponent.h" // 캡슐 컴포넌트 헤더 추가
 #include "Components/SkeletalMeshComponent.h" // 스켈레탈 메시 컴포넌트 헤더 추가
+#include "EnhancedInputComponent.h" // 향상된 입력 컴포넌트 헤더 추가
+#include "EnhancedInputSubsystems.h" // 향상된 입력 서브시스템 헤더 추가
+#include "GameFramework/PlayerController.h" // 플레이어 컨트롤러 헤더 추가
 
 // Sets default values
 ABird::ABird()
@@ -27,11 +30,33 @@ void ABird::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	// 입력 매핑 컨텍스트가 설정되어 있는지 확인
+	// 플레이어 컨트롤러를 가져와서 입력 매핑 컨텍스트를 추가
+	if (APlayerController* UnrealStudyPlayerController = Cast<APlayerController>(GetController()))
+	{
+		// 입력 매핑 컨텍스트를 플레이어 컨트롤러에 추가
+		// UEnhancedInputLocalPlayerSubsystem을 사용하여 입력 매핑 컨텍스트를 추가
+		if(UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(UnrealStudyPlayerController->GetLocalPlayer()))
+		{
+			// 입력 매핑 컨텍스트를 추가하여 입력을 처리할 수 있도록 설정
+			Subsystem->AddMappingContext(BirdMappingContext, 0);
+		}		
+	}
 }
 
 void ABird::MoveForward(float Value)
 {
 	UE_LOG(LogTemp, Warning, TEXT("MoveForward: %f"), Value);
+}
+
+// 입력 액션에 대한 처리 함수 구현
+void ABird::Move(const FInputActionValue& Value)
+{
+	const bool CurrentValue = Value.Get<bool>();
+	if (CurrentValue)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Move Action Triggered: %s"), *Value.ToString());
+	}
 }
 
 void ABird::Tick(float DeltaTime)
@@ -44,6 +69,13 @@ void ABird::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-	PlayerInputComponent->BindAxis(FName("MoveForward"), this, &ABird::MoveForward);
+	// 향상된 입력 시스템을 사용하여 입력 액션과 처리 함수를 바인딩
+	if(UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		// 입력 액션과 처리 함수를 바인딩하여 입력을 처리할 수 있도록 설정
+		EnhancedInputComponent->BindAction(MovedAction, ETriggerEvent::Triggered, this, &ABird::Move);
+	}
+
+	//PlayerInputComponent->BindAxis(FName("MoveForward"), this, &ABird::MoveForward);
 }
 
--- ch6 - unreal-advanced1/UnrealProject/UnrealStudy/Source/UnrealStudy/Public/Bird.h
+++ ch6 - unreal-advanced1/UnrealProject/UnrealStudy/Source/UnrealStudy/Public/Bird.h
@@ -4,10 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "InputActionValue.h" // FInputActionValue를 위해 추가
 #include "Bird.generated.h"
 
-class UCapsuleComponent;
-class USkeletalMeshComponent;
+class UCapsuleComponent; // 캡슐 컴포넌트 클래스 선언
+class USkeletalMeshComponent; // 스켈레탈 메시 컴포넌트 클래스 선언
+class UInputMappingContext; // 입력 매핑 컨텍스트 클래스 선언
+class UInputAction; // 입력 액션 클래스 선언
 
 UCLASS()
 class UNREALSTUDY_API ABird : public APawn
@@ -23,6 +26,17 @@ public:
 protected:
 	virtual void BeginPlay() override;
 	void MoveForward(float Value);
+
+	// 입력 매핑 컨텍스트 선언
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputMappingContext* BirdMappingContext;
+
+	// 입력 액션 선언
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* MovedAction;
+
+	// 입력 액션에 대한 처리 함수 선언
+	void Move(const FInputActionValue& Value);
 
 private:
 	// 캡슐 컴포넌트 선언
