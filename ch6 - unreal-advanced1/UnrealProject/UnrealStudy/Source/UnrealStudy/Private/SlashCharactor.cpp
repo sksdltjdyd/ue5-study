3c584c40c6f6e1706b235d4444d52cb17432a38b
SUROM <62508307+sksdltjdyd@users.noreply.github.com>
2026-06-10 08:41:12 +0900

    v 0.4.1

---
+++ ch6 - unreal-advanced1/UnrealProject/UnrealStudy/Source/UnrealStudy/Private/SlashCharactor.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SlashCharactor.h"
+
+// Sets default values
+ASlashCharactor::ASlashCharactor()
+{
+ 	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ASlashCharactor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASlashCharactor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ASlashCharactor::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
